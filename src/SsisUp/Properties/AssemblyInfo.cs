1e344167104cf48e5652b8b2c3fede9de2540c91
Joe Steed Mendonca <jsmendonca@gmail.com>
2015-04-12 08:51:02 +0100

    Added releasenotes file and readme

--- src/SsisUp/Properties/AssemblyInfo.cs
+++ src/SsisUp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SsisUp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A framework to automate deployments of SSIS using DbUp")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("JSM85")]
 [assembly: AssemblyProduct("SsisUp")]
